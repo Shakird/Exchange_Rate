bd660428d2cccb880a25eb69182493d5c36ead94
Shakir Davis <Shakir.Davis@gmail.com>
2020-02-11 17:21:13 -0500

    Version (1.0.0)
    Addition of project files.

---
+++ src/CurrencyRow.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function CurrencyRow(props) {
+
+    let getYear = () => new Date().getFullYear();
+    
+    const{
+        currencyOptions,
+        selectedCurrency,
+        onChangeCurrency,
+        onChangeAmount,
+        amount
+    } = props
+
+    return (
+        <div>
+            <input type="number" className='input' value = {amount} onChange={onChangeAmount}/>
+            <select value={selectedCurrency} onChange={onChangeCurrency}>
+                {currencyOptions.map(option => <option key = {option} value={option}>{option}</option>)}
+            </select>
+        </div>
+    )
+}
